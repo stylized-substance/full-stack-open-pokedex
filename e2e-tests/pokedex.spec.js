8454602a9a53d32afde381cb9373c49c1d580901
stylized-substance <116166387+stylized-substance@users.noreply.github.com>
2024-05-30 10:29:07 +0300

    update .eslintignore

--- e2e-tests/pokedex.spec.js
+++ e2e-tests/pokedex.spec.js
@@ -6,7 +6,7 @@ describe('Pokedex', () => {
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
-  test('it is possible to navigate to the page for Ivysaur from the front page', async ({page}) => {
+  test('it is possible to navigate to the page for Ivysaur from the front page', async ({ page }) => {
     await page.goto('/')
     await page.getByText('ivysaur').click()
     await expect(page.getByText('chlorophyll')).toBeVisible()
